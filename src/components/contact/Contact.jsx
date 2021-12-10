4f9d3b614c197957b050974a00e5dad8b1089db5
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-10 15:57:35 -0600

    display contact information

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import './contact.scss'
+import Phone from '../../img/phone.png'
+import Email from '../../img/email.png'
+import Address from '../../img/phone.png'
 
 function Contact() {
     return (
         <div className="c">
-            
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left">
+                    <h1 className="c-title">Let's dicuss your project!</h1>
+                    <div className="c-info">
+                        <div className="c-info-item">
+                            <img 
+                                src={Phone}
+                                alt="" 
+                                className="c-icon"
+                            />
+                            +1 913 944 6189
+                        </div>
+                        <div className="c-info-item">
+                            <img 
+                                src={Email}
+                                alt="" 
+                                className="c-icon"
+                            />
+                            Jeff.Woltjen@gmail.com
+                        </div>
+                        <div className="c-info-item">
+                            <img 
+                                src={Address}
+                                alt="" 
+                                className="c-icon"
+                            />
+                            github.com/JWoltjen
+                        </div>
+                    </div>
+                </div>
+                <div className="c-right"></div>
+            </div>
         </div>
     )
 }

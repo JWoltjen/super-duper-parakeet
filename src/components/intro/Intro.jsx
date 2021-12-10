55d21d9d136e5512c3d90ae93cf45fed76d2400f
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-10 10:21:54 -0600

    add job titles

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,9 +6,18 @@ function Intro() {
         <div className='i'>
             <div className="i-left">
                 <div className="i-left-wrapper">
-                        <h2 className="i-intro">Hello, My name is</h2>
-                        <h1 className="i-name">Jeff Woltjen</h1>
+                    <h2 className="i-intro">Hello, My name is</h2>
+                    <h1 className="i-name">Jeff Woltjen</h1>
+                    <div className="i-title">
+                        <div className='i-title-wrapper'>
+                            <div className='i-title-item'>Web Developer</div>
+                            <div className='i-title-item'>Digital Explorer</div>
+                            <div className='i-title-item'>Writer</div>
+                            <div className='i-title-item'>Content Developer</div>
+
+                        </div>
                     </div>
+                </div>
             </div>
             <div className="right"></div>
         </div>

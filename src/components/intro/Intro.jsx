28bb0cdfa19482372a8b9c0e264af0cdebbaf04a
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-10 10:19:29 -0600

    add segoe as global font

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,9 +3,14 @@ import "./intro.scss"
 
 function Intro() {
     return (
-        <div className='intro'>
-            <div className="left">left</div>
-            <div className="right">right</div>
+        <div className='i'>
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                        <h2 className="i-intro">Hello, My name is</h2>
+                        <h1 className="i-name">Jeff Woltjen</h1>
+                    </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }

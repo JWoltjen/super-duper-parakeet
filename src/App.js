89176e94e77ea9544331b9917ff37af40ee0cf0d
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-10 13:20:35 -0600

    create file structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
 import Intro from './components/intro/Intro';
 import About from './components/about/About';
+import Portfolio from './components/portfolio/Portfolio';
+import PortfolioList from './components/portfolioList/PortfolioList';
 
 function App() {
   return (
     <div className="App">
       <Intro/>
-      <About/>  
+      <About/>
+      {/* <Portfolio/> */}
+      <PortfolioList/>
     </div>
   );
 }
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,23 @@
+import './PortfolioList.scss'
+import Portfolio from '../portfolio/Portfolio'
+
+function PortfolioList() {
+    return (
+        <div className="pl">
+            <div className="pl-texts">
+                <h1 className="pl-title">Jeff: Driven to Discover.</h1>
+                <p className="pl-desc">
+                    Jeff is a hard worker and a smart guy. Eventually, he will build something great.
+                </p>
+            </div>
+            <div className="pl-list">
+                <Portfolio/>
+                <Portfolio/>
+                <Portfolio/>
+                <Portfolio/>
+            </div>
+        </div>
+    )
+}
+
+export default PortfolioList
